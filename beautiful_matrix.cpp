0a59c278c651a5d5b6106d1b7415ff2c96ce5b56
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-02-06 16:05:19 +0600

    Add files via upload

---
+++ beautiful_matrix.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int m,a[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            if(a[i][j]==1){
+                m=abs(i-2)+abs(j-2);
+            }
+        }
+    }
+    cout<<m;
+    return 0;
+}
